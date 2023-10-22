bd76590ffdb82424503d60687db59fdf8bff987d
newhyodong <hyodg90@gmail.com>
2023-10-23 00:00:22 +0900

    [Duplicated Code] Extract Function

--- src/main/java/me/whiteship/refactoring/_02_duplicated_code/_04_extract_function/StudyDashboard.java
+++ src/main/java/me/whiteship/refactoring/_02_duplicated_code/_04_extract_function/StudyDashboard.java
@@ -12,30 +12,41 @@ public class StudyDashboard {
 
     private void printParticipants(int eventId) throws IOException {
         // Get github issue to check homework
+        GHIssue issue = getGhIssue(eventId);
+
+        // Get participants
+        Set<String> participants = getUserNames(issue);
+
+        // Print participants
+        print(participants);
+    }
+
+    private void print(Set<String> participants) {
+        participants.forEach(System.out::println);
+    }
+
+    private Set<String> getUserNames(GHIssue issue) throws IOException {
+        Set<String> userNames = new HashSet<>();
+        issue.getComments().forEach(c -> userNames.add(c.getUserName()));
+        return userNames;
+    }
+
+    private GHIssue getGhIssue(int eventId) throws IOException {
         GitHub gitHub = GitHub.connect();
         GHRepository repository = gitHub.getRepository("whiteship/live-study");
         GHIssue issue = repository.getIssue(eventId);
-
-        // Get participants
-        Set<String> participants = new HashSet<>();
-        issue.getComments().forEach(c -> participants.add(c.getUserName()));
-
-        // Print participants
-        participants.forEach(System.out::println);
+        return issue;
     }
 
     private void printReviewers() throws IOException {
         // Get github issue to check reviews
-        GitHub gitHub = GitHub.connect();
-        GHRepository repository = gitHub.getRepository("whiteship/live-study");
-        GHIssue issue = repository.getIssue(30);
+        GHIssue issue = getGhIssue(30);
 
         // Get reviewers
-        Set<String> reviewers = new HashSet<>();
-        issue.getComments().forEach(c -> reviewers.add(c.getUserName()));
+        Set<String> reviewers = getUserNames(issue);
 
         // Print reviewers
-        reviewers.forEach(System.out::println);
+        print(reviewers);
     }
 
     public static void main(String[] args) throws IOException {
