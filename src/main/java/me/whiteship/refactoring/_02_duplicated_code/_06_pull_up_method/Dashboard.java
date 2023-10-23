a9429a8069b51e5511539268373310de0007d2ed
newhyodong <hyodg90@gmail.com>
2023-10-24 00:16:58 +0900

    [Duplicated Code] Pull Up Method

--- src/main/java/me/whiteship/refactoring/_02_duplicated_code/_06_pull_up_method/Dashboard.java
+++ src/main/java/me/whiteship/refactoring/_02_duplicated_code/_06_pull_up_method/Dashboard.java
@@ -1,7 +1,16 @@
 package me.whiteship.refactoring._02_duplicated_code._06_pull_up_method;
 
-import java.io.IOException;
+import org.kohsuke.github.GHIssue;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
 
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *  Incorporate similar methods from subclasses into the superclass
+ */
 public class Dashboard {
 
     public static void main(String[] args) throws IOException {
@@ -9,6 +18,21 @@ public class Dashboard {
         reviewerDashboard.printReviewers();
 
         ParticipantDashboard participantDashboard = new ParticipantDashboard();
-        participantDashboard.printParticipants(15);
+        participantDashboard.printUsernames(15);
+    }
+
+
+    public void printUsernames(int eventId) throws IOException {
+        // Get github issue to check homework
+        GitHub gitHub = GitHub.connect();
+        GHRepository repository = gitHub.getRepository("whiteship/live-study");
+        GHIssue issue = repository.getIssue(eventId);
+
+        // Get participants
+        Set<String> participants = new HashSet<>();
+        issue.getComments().forEach(c -> participants.add(c.getUserName()));
+
+        // Print participants
+        participants.forEach(System.out::println);
     }
 }
