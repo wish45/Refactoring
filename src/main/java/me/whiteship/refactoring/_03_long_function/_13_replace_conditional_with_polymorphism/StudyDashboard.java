9984b15b6219a7e8835b9e41959457ae9bf6c361
newhyodong <hyodg90@gmail.com>
2023-11-08 00:34:24 +0900

    [Long Function] replace conditional with polymorphism

--- src/main/java/me/whiteship/refactoring/_03_long_function/_13_replace_conditional_with_polymorphism/StudyDashboard.java
+++ src/main/java/me/whiteship/refactoring/_03_long_function/_13_replace_conditional_with_polymorphism/StudyDashboard.java
@@ -29,7 +29,7 @@ public class StudyDashboard {
 
     private void print() throws IOException, InterruptedException {
         checkGithubIssues(getGhRepository());
-        new StudyPrinter(this.totalNumberOfEvents, this.participants, PrinterMode.MARKDOWN).execute();
+        new MarkdownPrinter(this.totalNumberOfEvents, this.participants).execute(); //MarkdownPrinter or CvsPrinter, ConsolePrinter
     }
 
     private GHRepository getGhRepository() throws IOException {
