ff322359400808fc3da892d6e734af18b3a1a62b
newhyodong <hyodg90@gmail.com>
2023-11-20 00:38:02 +0900

    [mutable data] combine function into transform

--- src/main/java/me/whiteship/refactoring/_06_mutable_data/_22_combine_functions_into_transform/Reading.java
+++ src/main/java/me/whiteship/refactoring/_06_mutable_data/_22_combine_functions_into_transform/Reading.java
@@ -3,5 +3,11 @@ package me.whiteship.refactoring._06_mutable_data._22_combine_functions_into_tra
 import java.time.Month;
 import java.time.Year;
 
+/**
+ *
+ * record
+ * final, invoke once , no setter
+ */
+
 public record Reading(String customer, double quantity, Month month, Year year) {
 }
