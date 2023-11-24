a7cf4c3f221c21b23d6ef67d711dbed6755e43d7
newhyodong <hyodg90@gmail.com>
2023-11-24 23:18:32 +0900

    [mutable data] change reference to value

--- src/main/java/me/whiteship/refactoring/_06_mutable_data/_23_change_reference_to_value/TelephoneNumber.java
+++ src/main/java/me/whiteship/refactoring/_06_mutable_data/_23_change_reference_to_value/TelephoneNumber.java
@@ -1,24 +1,40 @@
 package me.whiteship.refactoring._06_mutable_data._23_change_reference_to_value;
 
+import java.util.Objects;
+
+/**
+ * public record TelephoneNumber(String areaCode, String number){}
+ * 와같이 record로 이용한 간단한 방법 사용도 가능하다(JDK 17이상).
+ */
 public class TelephoneNumber {
 
-    private String areaCode;
+    private final String areaCode;
 
-    private String number;
+    private final String number;
+
+    public TelephoneNumber(String areaCode, String number) {
+        this.areaCode = areaCode;
+        this.number = number;
+    }
 
     public String areaCode() {
         return areaCode;
-    }
-
-    public void areaCode(String areaCode) {
-        this.areaCode = areaCode;
     }
 
     public String number() {
         return number;
     }
 
-    public void number(String number) {
-        this.number = number;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TelephoneNumber that = (TelephoneNumber) o;
+        return Objects.equals(areaCode, that.areaCode) && Objects.equals(number, that.number);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(areaCode, number);
     }
 }
