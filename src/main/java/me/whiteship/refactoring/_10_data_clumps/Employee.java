08079db1b359829fa506ea89de80fa6703d5ee64
newhyodong <hyodg90@gmail.com>
2023-12-17 23:56:42 +0900

    [data clumps] data clumps

--- src/main/java/me/whiteship/refactoring/_10_data_clumps/Employee.java
+++ src/main/java/me/whiteship/refactoring/_10_data_clumps/Employee.java
@@ -4,18 +4,15 @@ public class Employee {
 
     private String name;
 
-    private String personalAreaCode;
+    private TelephoneNumber personalPhoneNumber;
 
-    private String personalNumber;
-
-    public Employee(String name, String personalAreaCode, String personalNumber) {
+    public Employee(String name, TelephoneNumber personalPhoneNumber) {
         this.name = name;
-        this.personalAreaCode = personalAreaCode;
-        this.personalNumber = personalNumber;
+        this.personalPhoneNumber = personalPhoneNumber;
     }
 
     public String personalPhoneNumber() {
-        return personalAreaCode + "-" + personalNumber;
+        return this.personalPhoneNumber.toString();
     }
 
     public String getName() {
@@ -26,19 +23,7 @@ public class Employee {
         this.name = name;
     }
 
-    public String getPersonalAreaCode() {
-        return personalAreaCode;
-    }
-
-    public void setPersonalAreaCode(String personalAreaCode) {
-        this.personalAreaCode = personalAreaCode;
-    }
-
-    public String getPersonalNumber() {
-        return personalNumber;
-    }
-
-    public void setPersonalNumber(String personalNumber) {
-        this.personalNumber = personalNumber;
+    public TelephoneNumber getPersonalPhoneNumber() {
+        return personalPhoneNumber;
     }
 }
