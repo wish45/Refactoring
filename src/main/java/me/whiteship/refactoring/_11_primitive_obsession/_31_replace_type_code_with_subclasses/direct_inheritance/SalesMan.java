14c5d32a46c38994d06ba5d80029c7157e6a562e
newhyodong <hyodg90@gmail.com>
2023-12-27 23:19:17 +0900

    [primitive obssesion] replace type code with subclasses

---
+++ src/main/java/me/whiteship/refactoring/_11_primitive_obsession/_31_replace_type_code_with_subclasses/direct_inheritance/SalesMan.java
@@ -0,0 +1,12 @@
+package me.whiteship.refactoring._11_primitive_obsession._31_replace_type_code_with_subclasses.direct_inheritance;
+
+public class SalesMan extends Employee{
+    public SalesMan(String name) {
+        super(name);
+    }
+
+    @Override
+    public String getType() {
+        return "salesMan";
+    }
+}
