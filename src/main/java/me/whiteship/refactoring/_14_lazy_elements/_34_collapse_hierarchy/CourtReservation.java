0d26a8527baf070f4198ad9855cffa6e824f69f6
newhyodong <hyodg90@gmail.com>
2024-01-05 22:54:15 +0900

    [lazy element] collapse hierarchy - pull down or pull up

--- src/main/java/me/whiteship/refactoring/_14_lazy_elements/_34_collapse_hierarchy/CourtReservation.java
+++ src/main/java/me/whiteship/refactoring/_14_lazy_elements/_34_collapse_hierarchy/CourtReservation.java
@@ -1,7 +1,15 @@
 package me.whiteship.refactoring._14_lazy_elements._34_collapse_hierarchy;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class CourtReservation extends Reservation {
 
     private String courtNumber;
 
+    private LocalDateTime startDateTime;
+    private LocalDateTime endDateTime;
+    private List<String> members;
+    private String owner;
+    private boolean paid;
 }
