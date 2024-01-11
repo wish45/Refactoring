10e55eb92a5a25bca2fa622bc0e26e796c89eb79
newhyodong <hyodg90@gmail.com>
2024-01-11 21:38:28 +0900

    [temporary field] introduce special case - add

--- src/main/java/me/whiteship/refactoring/_16_temporary_field/_36_introduce_special_case/UnknownCustomer.java
+++ src/main/java/me/whiteship/refactoring/_16_temporary_field/_36_introduce_special_case/UnknownCustomer.java
@@ -2,6 +2,7 @@ package me.whiteship.refactoring._16_temporary_field._36_introduce_special_case;
 
 public class UnknownCustomer extends Customer{
 
+    
     public UnknownCustomer() {
         super("unknown",new BasicBillingPlan(),new NullPaymentHistory());
     }
